cf2c82cbc08546e4056185b6c660d31d554d5f7a
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-01 11:55:19 +0000

    Refactor App component to a class
    App component will be responsible for maintaining the state of the application, hence it was refactored to a class.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import React from 'react';
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>
-          CV Application
-        </h1>
-      </header>
-      <main></main>
-      <footer>
-        <a href='https://github.com/MuKhAlt/cv-application.git'>
-          <i className='fa-github fab'></i>
-        </a>
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header>
+          <h1>
+            CV Application
+          </h1>
+        </header>
+        <main>
+          <div className='creation'>
+          </div>
+        </main>
+        <footer>
+          <a href='https://github.com/MuKhAlt/cv-application.git'>
+            <i className='fa-github fab'></i>
+          </a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
