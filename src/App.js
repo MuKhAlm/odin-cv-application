4ea2428451e341aed57643b7c34173d85507d1c8
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-17 21:03:47 +0000

    Create state in App and link it to PersonalDetails
    PersonalDetails input is now controlled by the App's state and changes to it are reflected in the state of App.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import './App.css'
 
 import PersonalDetails from './components/PersonalDetails';
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      personalDetails: createPersonalDetails({
+        firstName: '',
+        surname: '',
+        email: '',
+        phoneNumber: '',
+        description: ''
+    })
+    }
+  }
+
+  /**
+   * Handles user input for Personal Details field,
+   * 
+   * Sets the appropriate Personal Detail property to what the user inputted,
+   * 
+   * @param {Object} e Event
+   * @modifies this.state (this = App)
+   */
+  handlePersonalDetailsChange = (e) => {
+    let pd = createPersonalDetails(this.state.personalDetails)
+    pd[e.target.name] = e.target.value
+
+    this.setState({
+      personalDetails: pd
+    })
+  }
+
   render () {
     return (
       <div className="App">
@@ -15,7 +46,14 @@ class App extends React.Component {
         <main>
           <div id='production'>
             <h2>Personal Details</h2>
-            <PersonalDetails></PersonalDetails>
+            <PersonalDetails 
+              firstName={this.state.personalDetails.firstName}
+              surname={this.state.personalDetails.surname}
+              email={this.state.personalDetails.email}
+              phoneNumber={this.state.personalDetails.phoneNumber}
+              description={this.state.personalDetails.description}
+              onChange = {this.handlePersonalDetailsChange}
+            ></PersonalDetails>
           </div>
         </main>
         <footer>
@@ -28,4 +66,19 @@ class App extends React.Component {
   }
 }
 
-export default App;
+/**
+ * Creates a Personal Details Object and returns it,
+ * 
+ * @param {Obejct} PD A Personal Details Object.
+ *                    Its structure is {
+ *                      firstName: {String},
+ *                      surname: {String},
+ *                      email: {String},
+ *                      phoneNumber: {String} (Only numbers),
+ *                      description: {String}
+ *                    }
+ * @returns New Personal Detials Object
+ */
+function createPersonalDetails(PD) {
+  return Object.assign({}, PD)
+}
--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -5,11 +5,45 @@ export default class PersonalDetails extends React.Component {
   render() {
     return (
       <div id='PersonalDetails'>
-        <input id="firstNameInput" type={"text"} placeholder={"First Name"}></input>
-        <input id="surnameInput" type={"text"} placeholder={"Surname"}></input>
-        <input id="emailInput" type={"email"} placeholder={"Email"}></input>
-        <input id="phoneNumberInput" type={"number"} placeholder={"Phone Number"}></input>
-        <textarea id="descriptionInput" placeholder="Description"></textarea>
+        <input 
+          id="firstNameInput"
+          name='firstName' 
+          type={"text"} 
+          placeholder={"First Name"}
+          value={this.props.firstName}
+          onChange={this.props.onChange}
+        ></input>
+        <input 
+          id="surnameInput"
+          name='surname'  
+          type={"text"} 
+          placeholder={"Surname"}
+          value={this.props.surname}
+          onChange={this.props.onChange}
+        ></input>
+        <input 
+          id="emailInput" 
+          name='email' 
+          type={"email"}
+          placeholder={"Email"}
+          value={this.props.email}
+          onChange={this.props.onChange}
+        ></input>
+        <input 
+          id="phoneNumberInput"
+          name='phoneNumber' 
+          type={"number"} 
+          placeholder={"Phone Number"}
+          value={this.props.phoneNumber}
+          onChange={this.props.onChange}
+        ></input>
+        <textarea 
+          id="descriptionInput" 
+          name='description'
+          placeholder="Description"
+          value={this.props.description}
+          onChange={this.props.onChange}
+        ></textarea>
       </div>
     )
   }
